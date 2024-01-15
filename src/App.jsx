ddb3377d5c4d180a3d28d8ce9b32478858a4edad
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-15 20:07:32 +0530

    experience component  partially done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Skills from './components/Skills'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
+import Experience from './components/Experience'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Hero/>
       <Skills/>
       <Projects/>
+      <Experience/>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen'>Hero</div>
+    <div className='p-20 w-full h-screen'>
+        <div className='w-full h-full p-10 bg-red-700'>
+            <header>
+                <h3 className='text-2xl font-semibold'>Hi</h3>
+                <h1></h1>
+            </header>
+        </div>
+    </div>
   )
 }
 
