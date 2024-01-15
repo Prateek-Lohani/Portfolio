3cd03637ddbb7ba99d5c24507a6a15f5f6823157
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-16 01:11:29 +0530

    iframes done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                     <h1 className='text-white text-center'>Intro</h1>
                 </div>
                 
-                <div className='bg-red-400 w-[80vw] h-[45vh] md:w-[60%] md:h-[62vh]'>
+                <div className='w-[80vw] h-[45vh] md:w-[60%] md:h-[62vh]'>
                 <iframe className='w-full h-full' src="https://lottie.host/embed/e1230349-da1c-411e-b237-45eced39c5e2/1SC1cEmtuB.lottie"></iframe>
                 </div>
             </header>
