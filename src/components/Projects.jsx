8db1418a57628687330174289cb9aff24f49136f
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-16 14:21:08 +0530

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,10 +14,10 @@ const Projects = () => {
             Creating Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
         </div>
-        <div  className='w-[90%] md:w-[80%] h-[90%] md:p-8 p-4 mx-auto bg-slate-500/20 relative border border-cyan-400 rounded-lg md:flex md:justify-center md:items-center flex-wrap md:gap-8 gap-6'>
+        <div  className='w-[90%] md:w-[80%] h-[90%] md:p-8 p-4 mx-auto bg-slate-500/20 relative border border-cyan-400 rounded-lg flex md:justify-center md:items-center md:flex-wrap md:gap-8 gap-6 overflow-x-auto md:overflow-hidden' id='projectscroll'>
       
     {projects.map((project)=>{
-      return <div className='w-full h-[50vh] md:p-4 md:h-[80vh] bg-red-700 md:flex'>
+      return <div className='w-full h-[50vh] md:p-4 md:h-[80vh] bg-red-700 md:flex shrink-0'>
       <div className='left h-[70%] md:w-[65%] md:h-full bg-yellow-500 flex justify-center items-center'>
           <video src={project.vidsrc} autoPlay='true' muted='true' loop className='w-full scale-y-[2] md:scale-y-[1.3] object-cover md:h-full md:object-contain'></video>
       </div>
