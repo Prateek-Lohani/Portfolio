778e9674cb84758f7290d8b125100e6b658ffa35
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-18 19:20:29 +0530

    exp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,13 +6,13 @@ const Form = () => {
   const {register,reset,handleSubmit,formState:{errors}}=useForm();
 
   return (
-    <form className='w-full h-full bg-black/90 px-4 py-4 md:px-20 md:py-10 rounded-md md:rounded-xl'>
-        <div className='md:pb-8 mb-4 container text-sm flex gap-4 md:gap-6 w-full'>
+    <form className='w-full h-fit bg-black/90 px-4 py-4 md:px-20 md:py-10 rounded-md md:rounded-xl'>
+        <div className='md:pb-2 mb-4 container text-xs md:text-sm flex gap-4 md:gap-6 w-full'>
           <input type='text' className='w-[40%] h-[6vh] rounded px-2 md:px-8' placeholder='Your Name'/>
           <input type='text' className='w-[60%] h-[6vh] rounded px-2 md:px-8' placeholder='Your Email'/>
         </div>
-        <textarea className='md:mb-6 text-sm mb-4 resize-none w-full h-[60%] px-2 md:px-8 py-2 rounded' placeholder='Enter Your Message' />
-        <button className='w-[100%] h-[15%] bg-cyan-800 rounded-xl text-md md:text-2xl text-white' type='submit'>Send</button>
+        <textarea className='md:mb-4 text-xs md:text-sm resize-none w-full h-[40vh] px-2 md:px-8 py-2 rounded' placeholder='Enter Your Message' />
+        <button className='w-[100%] h-[8vh] bg-cyan-800 rounded-xl text-md md:text-2xl text-white' type='submit'>Send</button>
     </form>
   )
 }
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -22,14 +22,14 @@ const data=[
  const [socials,setSocials]=useState(data);
  
     return (
-    <div ref={refer} className='w-full bg-cyan-600/70 md:pt-[10vh] pb-0 p-4 md:h-fit  flex flex-col items-center justify-center relative'>
+    <div ref={refer} className='w-full bg-cyan-600/70 md:pb-4 md:pt-[10vh] pb-0 p-4 md:h-fit  flex flex-col items-center justify-center relative'>
         <h5 className='text-4xl text-white md:text-8xl font-extrabold p-2 font-[circular]'>Get in touch</h5>
         
-        <div className="form-container md:w-[50%] w-[80vw] h-[60vh] md:h-[70vh] ">
+        <div className="form-container md:w-[50%] w-[80vw] h-fit md:h-[70vh] ">
             <Form/>
         </div>
 
-        <div className="md:hidden pt-2 pb-1 flex justify-center gap-4 md:gap-10 items-top">
+        <div className="md:hidden pt-6 pb-2 flex justify-center gap-4 md:gap-10 items-top">
                 {socials.map((social)=>{
                     return (<div key={social.id} className='w-[6%] rounded-xl  overflow-hidden'>
                         <img onClick={()=>window.open(`${social.href}`)} className='hover:cursor-pointer hover:scale-110 transition-all w-full rounded-md overflow-hidden  h-full object-cover bg-white' src={social.icon} alt={social.icon}/>
