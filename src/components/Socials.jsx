d4a693e5029350ad17cdcfdf679a21ec2fa57b1c
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-18 19:29:04 +0530

    exp

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -25,7 +25,7 @@ const data=[
     <div ref={refer} className='w-full bg-cyan-600/70 md:pb-4 md:pt-[10vh] pb-0 p-4 md:h-fit  flex flex-col items-center justify-center relative'>
         <h5 className='text-4xl text-white md:text-8xl font-extrabold p-2 font-[circular]'>Get in touch</h5>
         
-        <div className="form-container md:w-[50%] w-[80vw] h-fit md:h-[70vh] ">
+        <div className="form-container md:w-[50%] w-[90vw] h-fit md:h-[70vh] ">
             <Form/>
         </div>
 
