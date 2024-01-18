54f819c56ed55ac0b248d467fd5769feb9c47881
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-18 19:21:55 +0530

    exp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ const Form = () => {
           <input type='text' className='w-[40%] h-[6vh] rounded px-2 md:px-8' placeholder='Your Name'/>
           <input type='text' className='w-[60%] h-[6vh] rounded px-2 md:px-8' placeholder='Your Email'/>
         </div>
-        <textarea className='md:mb-4 text-xs md:text-sm resize-none w-full h-[40vh] px-2 md:px-8 py-2 rounded' placeholder='Enter Your Message' />
+        <textarea className='md:mb-4 text-xs md:text-sm resize-none w-full h-[40vh] mb-4 px-2 md:px-8 py-2 rounded' placeholder='Enter Your Message' />
         <button className='w-[100%] h-[8vh] bg-cyan-800 rounded-xl text-md md:text-2xl text-white' type='submit'>Send</button>
     </form>
   )
