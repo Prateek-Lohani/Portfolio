8b8dd2c9c278233f1656c7bafa0dab9a21e3aef9
Prateek Lohani <prateeklohani105@gmail.com>
2024-01-23 23:06:21 +0530

    hero completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -76,7 +76,7 @@ const Skills = () => {
         <div className='flex justify-center items-center flex-col text-white'>
         <h5 className='text-4xl md:text-9xl font-[900] p-2 '>Skills</h5>
         
-        <p className='pb-8 md:pb-12 md:w-[50%] w-[75%] text-center text-sm md:text-2xl py-6  tracking-tight font-normal'>
+        <p className='pb-8 md:pb-12 md:w-[50%] w-[80%] text-center text-[4vw] md:text-2xl py-6  font-normal'>
             You can't box these skills into a simple list, they are an explosion display of talent in action. But for the sake of formality, check the skills below.
         </p>
 
