03af1e871a8cfdce135b400329d92798be58ee0d
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2025-05-19 23:01:14 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-[3vh] md:h-[5vh] bg-cyan-600/70 flex justify-center items-end md:items-center text-xs md:text-sm text-white'>Copyright © Prateek Lohani - 2024</div>
+    <div className='w-full md:mt-4 h-[3vh] md:h-[5vh] bg-cyan-600/70 flex justify-center items-end md:items-center text-xs md:text-sm text-white'>Copyright © Prateek Lohani - 2024</div>
   )
 }
 
