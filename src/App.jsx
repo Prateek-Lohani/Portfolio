07dc99c60d73b3e5f56648bef9d0c0832b46d6b7
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2026-01-20 19:42:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const scrollToSocials=()=>{
 
   return (
     <Suspense fallback={<Loading />}>
-      <div className='w-full'>
+      <div className='w-full bg-[#0a0a0a]'>
         <Nav scrollToSkills={scrollToSkills} scrollToExperience={scrollToExperience} scrollToResume={scrollToResume} scrollToProjects={scrollToProjects} scrollToSocials={scrollToSocials}/>
         <Hero />
         <Skills refer={skillRef}/>
